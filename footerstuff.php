8dbdb2dd843c751e59cffc0ac4502153cddfe8ce
weefselkweekje <weefselkweekje@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-12-22 11:31:06 +0000

    git-svn-id: https://plugins.svn.wordpress.org/footer-stuff/trunk@81747 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ footerstuff.php
@@ -0,0 +1,79 @@
+<?php
+/*
+Plugin Name: Footer Stuff
+Plugin URI: http://www.roytanck.com
+Description: Maintain things like stats code from within WordPress without having to alter your theme's footer file.
+Version: 1.0
+Author: Roy Tanck
+Author URI: http://www.roytanck.com
+
+Copyright Roy Tanck  (email : roy.tanck@gmail.com)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+// insert code into footer
+function footerstuff() {
+	$options = get_option('footerstuff_options');
+	echo $options['footertext'];
+}
+
+// insert code into header
+function headerstuff() {
+	$options = get_option('footerstuff_options');
+	echo $options['headertext'];
+}
+
+// options page
+function footerstuff_options() {
+	$options = $newoptions = get_option('footerstuff_options');
+	// if submitted, process results
+	if ( $_POST["footerstuff_submit"] ) {
+		$newoptions['footertext'] = stripslashes($_POST["footertext"]);
+		$newoptions['headertext'] = stripslashes($_POST["headertext"]);
+	}
+	// any changes? save!
+	if ( $options != $newoptions ) {
+		$options = $newoptions;
+		update_option('footerstuff_options', $options);
+	}
+	// options form
+	echo '<form method="post">';
+	echo "<div class=\"wrap\"><h2>Footer Stuff</h2>";
+	echo '<table class="form-table">';
+	// footer text
+	echo '<tr valign="top"><th scope="row">HTML code to insert into the footer</th>';
+	echo '<td><textarea style="width: 98%; font-size: 12px;" class="code" name="footertext" id="footertext" cols="60" rows="25">'.$options['footertext'].'</textarea></td></tr>';
+	// headerer text
+	echo '<tr valign="top"><th scope="row">HTML code to insert into the header</th>';
+	echo '<td><textarea style="width: 98%; font-size: 12px;" class="code" name="headertext" id="headertext" cols="60" rows="10">'.$options['headertext'].'</textarea></td></tr>';
+	// close stuff
+	echo '<input type="hidden" name="footerstuff_submit" value="true"></input>';
+	echo '</table>';
+	echo '<p class="submit"><input type="submit" value="Update Options &raquo;"></input></p>';
+	echo "</div>";
+	echo '</form>';
+}
+
+
+function footerstuff_add_pages() {
+	add_submenu_page('themes.php', 'Footer Stuff', 'Footer Stuff', 10, __FILE__, 'footerstuff_options');
+}
+
+add_action('wp_footer', 'footerstuff');
+add_action('wp_head', 'headerstuff');
+add_action('admin_menu', 'footerstuff_add_pages');
+
+?>
---
+++ readme.txt
@@ -0,0 +1,41 @@
+=== Plugin Name ===
+Contributors: weefselkweekje
+Donate link: http://www.roytanck.com/about-my-themes/donations/
+Tags: footer, code snippets, manage, analytics, statistics
+Requires at least: 2.5
+Tested up to: 2.6.5
+Stable tag: 1.0
+
+Manage footer code snippets from within WordPress. No need to edit your theme's footer.php file any more.
+
+== Description ==
+
+Footer Stuff allows you to manage footer code snippets (such as those for stats/analytics) from within WordPress. It eliminates the need to copy these snippets into your theme's PHP files, and lets you edit them through an options page located under WordPress' design tab. This makes it easier to edit these snippets and to switch themes. Version 1.0 lets you add stuff to the page's header as well.
+
+== Installation ==
+
+To install Footer Stuff, follow the steps below.
+
+1. Upload the 'footer-stuff' folder to the '/wp-content/plugins/' directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Manage the code snippets in at 'Design'->'Footer Stuff'
+
+== Frequently Asked Questions ==
+
+= Why would I want to use this instead of editing my theme's footer.php file? =
+
+For one thing, you wont's have to worry about these snippets when you switch themes. PHP files are also easy to mess up, so editing them can be tricky.
+
+= Why would I want to use this instead of a dedicated plugin for mey stats system? =
+
+If you're using multiple stats systems, Footer Stuff lets you add the code in one place. And it lets add other things as well. If you know your way around HTML this is probably more convenient for you.
+
+== Screenshots ==
+
+1. The admin interface
+
+== Version history ==
+
+= Version 1.0 =
+
+Initial release.
